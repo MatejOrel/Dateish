7728860a9084f87119e4bb516bc5904569a04c48
MATEJ-LAPTOP\Matej <matej.orel123@gmail.com>
2020-12-04 17:35:35 +0100

    neki_login

--- source/app/src/main/java/com/example/dateish/LoginActivity.java
+++ source/app/src/main/java/com/example/dateish/LoginActivity.java
@@ -1,19 +1,17 @@
 package com.example.dateish;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_login); mAuth = FirebaseAuth.getInstance();
 
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
