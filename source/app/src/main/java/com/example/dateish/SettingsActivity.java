5d9acf2cc22a449e0b5cce3714e6eb068dfe2460
MATEJ-LAPTOP\Matej <matej.orel123@gmail.com>
2020-12-08 17:48:21 +0100

    scrollable

--- source/app/src/main/java/com/example/dateish/SettingsActivity.java
+++ source/app/src/main/java/com/example/dateish/SettingsActivity.java
@@ -1,9 +1,5 @@
 package com.example.dateish;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,11 +8,14 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -125,12 +124,10 @@ public class SettingsActivity extends AppCompatActivity {
                             mMale.performClick();
 
                         }
-                           // mChooseSex.check(R.id.men);
                         else{
                             mFemale = mChooseSex.findViewById(R.id.women);
                             mFemale.performClick();
                         }
-                           // mChooseSex.check(R.id.women);
                     }
                 }
             }
